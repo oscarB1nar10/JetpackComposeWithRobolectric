68f384a6ee13434cecd8eab33d1131e8b3f86f04
Oscar <oscivaramtin@gmail.com>
2022-11-15 12:51:29 -0500

    rb/Import_dependencies
    Some missing dependencies were included

---
+++ app/src/test/java/com/oscar/jetpackcomposewithrobolectric/ExampleRobolectricTest.kt
@@ -0,0 +1,20 @@
+package com.oscar.jetpackcomposewithrobolectric
+
+import androidx.compose.ui.test.junit4.createComposeRule
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.robolectric.RobolectricTestRunner
+
+@RunWith(RobolectricTestRunner::class)
+class ExampleRobolectricTest {
+
+    @get:Rule
+    val composeTestRule = createComposeRule()
+
+
+    @Test
+    fun `test screen text`() {
+
+    }
+}
